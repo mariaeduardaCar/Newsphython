04f16254a6adaeebd0716c195ce67c0b0f5bd357
mariaeduardaCar <SeuEmailDeGitHub>
2025-02-02 17:54:57 -0300

    Atualizações antes de enviar para o Heroku

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Carrega variáveis do .env
 
-DATABASE_URL = "postgresql://postgres:Fe151206@localhost:5432/noticias"
+DATABASE_URL = "postgres://uas8ulgphgk6um:p2678d698162c89550ed965286842fe34da8c45a48d040db374cb5862b94250e3@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/ddf6eu4nbjhhp2"
 SECRET_KEY = "12345"
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
